5ca7020d9e6a9dfe5b182415495f8399dd3b828c
Afonso Raposo Pereira <up201505870@feup-b213d23.fe.up.pt>
2019-03-13 13:27:32 +0000

    Aula 4 Finished

---
+++ Problem_Set_3/ex5.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <wait.h>
+
+int main(int argc, char *argv[]) {
+
+    if(argc < 2) {
+        printf("Wrong use of program.\n");
+        return -1;
+    }
+
+    int stat;
+    char *const parmList[] = {"/usr/bin/gcc", argv[1], NULL};
+
+    execv("/usr/bin/gcc", parmList);
+
+    return 0;
+}
